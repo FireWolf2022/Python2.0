d1fe0d8e50238c8303cbe28c6561c7416a70c103
Leonel <pinoleo626@gmail.com>
2025-11-06 18:47:42 -0500

    Argumentos indefinidos

--- argumentos indefinidos.py
+++ argumentos indefinidos.py
@@ -13,3 +13,12 @@ print(suma(4,5,6,45,-34,7))
 
 print(potencia(2, 2, 2, 2, 5))
 print("Probando desde PyCharm por segunda vez")
+
+def new(*cositas):
+    for i in cositas:
+        if i %2 == 0:
+            print(f"{i} es par")
+        else:
+            print(f"{i} es impar")
+
+new(1,2,3,4,5,6,7,8,9,10)
